4fbd9928da27cc89bf85775554416ceb73044db8
zunaedL <mdzunaed4@gmail.com>
2024-05-23 22:17:05 +0600

    show django data on html template v4,14:30

--- home/views.py
+++ home/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home/index.html")
+    peoples = [
+        {'name': 'Zunayed', 'age': 21},
+        {'name': 'Zubaer', 'age': 25},
+        {'name': 'Mokhles', 'age': 16},
+        {'name': 'Salam', 'age': 17},
+        {'name': 'Borkot', 'age': 65},
+    ]
+    return render(request, "home/index.html",context= {'peoples': peoples })
 
 def success_page(request):
     return HttpResponse("This is success page")
